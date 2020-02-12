9c7369a3cdba9d1639e59fb5ddf2589ec7e4511f
John Ojetunde <john.ojetunde@telesoftas.com>
2020-02-12 17:54:11 +0200

    chore: renamed the FunctionUtils to FunctionUtil

--- src/main/java/com/revolut/transfer/domain/util/FunctionUtil.java
+++ src/main/java/com/revolut/transfer/domain/util/FunctionUtil.java
@@ -4,13 +4,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class FunctionUtils {
+public class FunctionUtil {
     public static boolean isNullOrEmpty(String st) {
         return (st == null || st.isEmpty());
     }
 
-    public static <T> List<T> getAllAsList(Map<String, T> database) {
-        return database.entrySet()
+    public static <T> List<T> getAllAsList(Map<String, T> datasource) {
+        return datasource.entrySet()
                 .parallelStream()
                 .map(Map.Entry::getValue)
                 .collect(Collectors.toList());
